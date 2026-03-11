da925fa3d9c1270b9f8c607d58f0695f374d0c45
Keyshawn Felton <70709247+Keyshawn4@users.noreply.github.com>
2026-03-11 15:07:01 -0400

    Version 3 -- uses chroma database to locally query embeddings to find relevant chunks

--- document_QnA_tool.py
+++ document_QnA_tool.py
@@ -1,31 +1,22 @@
 import pymupdf
 import pymupdf4llm
-import pathlib
 from pathlib import Path
 import os
 import anthropic
 from anthropic import Anthropic
 from dotenv import load_dotenv
-from operator import itemgetter
 import hashlib
-import numpy as np
-import json
-from transformers import logging as transformers_logging
-from transformers.utils import logging
+import chromadb
+from chromadb import Collection
 
 load_dotenv()
-
-logging.disable_progress_bar()
-logging.get_logger("transformers").setLevel(logging.ERROR)
-logging.get_logger("huggingface_hub").setLevel(logging.ERROR)
-
 ACCEPTED_FILE_TYPES = [".pdf", ".xps", ".epub", ".mobi", ".fb2", ".cbz", ".svg", ".txt", ".doc", ".docx"]
 SYSTEM_PROMPT = "You are a helpful, kind, and thoughtful assistant helping the user answer questions they have about a document.\
     You should do your best to answer the questions posed to you by the user, but if you do not know the answer, likely because \
     the answer is not in the information, you should let the user know that you do not have an answer but try to help the user by\
     telling them what information would be needed to answer the question. You should not give answers that are not relevant to the\
     context received or use outside information to guess at an answer. Stick strictly to the context provided."
-CHUNK_THRESHOLD = 0.25
+CHUNK_THRESHOLD = 1.75
 TOP_N = 5
 
 # Convert document to markdown format in chunks
@@ -46,30 +37,18 @@ def doc_to_chunks(path: Path) -> list:
     return chunks
 
 
-def find_relevant_chunks(transformer_model, question: str, chunk_data: list) -> list:
-    relevant_chunks = []
-    threshold_chunks = [] # for chunks that have passed the minimum threshold
-    question_embedding = transformer_model.encode(question, show_progress_bar=False)
-    for chunk in chunk_data:
-        similarity_score = transformer_model.similarity(question_embedding, chunk[1])
-        if similarity_score >= CHUNK_THRESHOLD:
-            threshold_chunks.append((chunk[0], chunk[1], similarity_score))
+def find_relevant_chunks(chroma_collection: Collection, question:str) -> dict:
+    temp_query_results = chroma_collection.query(
+        query_texts=[question],
+        n_results=TOP_N
+    )
+    return [doc for doc, dist in zip(temp_query_results["documents"][0], temp_query_results["distances"][0]) if dist <= CHUNK_THRESHOLD]
 
-    threshold_chunks = sorted(threshold_chunks, key=itemgetter(2), reverse=True)
-    for i in range(TOP_N):
-        if i < len(threshold_chunks):
-            relevant_chunks.append(threshold_chunks[i][0])
-        else:
-            break
-    
-    return relevant_chunks
-
-def query_document(transformer_model, chunks: list, document_path: Path):
+def query_document(chroma_collection: Collection, chunks: list, document_path: Path):
     client = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
     context_window_limit = 200000
     context_threshold = 0.85
     model = "claude-haiku-4-5"
-    chunk_data = [(chunk, transformer_model.encode(chunk, show_progress_bar=False)) for chunk in chunks]
     while (True):
         question = input("Please ask your question: ")
         if (question == "quit"):
@@ -79,8 +58,7 @@ def query_document(transformer_model, chunks: list, document_path: Path):
             print("No message received")
             continue
 
-        chunk_text_embeddings = compute_embeddings(transformer_model, question, document_path, chunk_data)
-        relevant_chunks = "\n\n".join(find_relevant_chunks(transformer_model, question, chunk_text_embeddings))
+        relevant_chunks = "\n\n".join(find_relevant_chunks(chroma_collection, question))
 
         if not relevant_chunks:
             print("No relevant context can be found, please try again")
@@ -124,27 +102,13 @@ def query_document(transformer_model, chunks: list, document_path: Path):
 def hash_document_path(document_path: Path) -> str:
     return hashlib.sha256(str(document_path).encode()).hexdigest()
 
-def compute_embeddings(transformer_model, question: str, document_path: Path, chunk_data: list):
-    target_directory = Path("embeddings_cache")
-    if not target_directory.is_dir():
-        target_directory.mkdir(parents=True, exist_ok=True)
-    document_hash = hash_document_path(document_path)
-    cache_path = pathlib.Path(target_directory / document_hash)
-    embeddings_file_path = cache_path.with_suffix('.npy')
-    chunk_file_path = cache_path.with_suffix('.json')
+def compute_embeddings(chroma_collection: Collection, chunks:list, document_path: str):
+    chunk_ids = [hash_document_path(document_path) + f"{i}" for i in range(len(chunks))]
 
-    if not embeddings_file_path.exists():
-        chunk_text, chunk_embeddings = zip(*chunk_data)
-        with chunk_file_path.open('w') as file:
-            json.dump(chunk_text, file)
-        np.save(embeddings_file_path, chunk_embeddings)
-        return chunk_data
-    else:
-        with open(chunk_file_path,'r') as file:
-            chunk_text = json.load(file)
-        chunk_embeddings = np.load(embeddings_file_path)
-        chunk_data = zip(chunk_text, chunk_embeddings)
-        return chunk_data
+    chroma_collection.upsert(
+        ids=chunk_ids,
+        documents=chunks,
+    )
 
 def main():
     # Get document path and get document
@@ -152,9 +116,10 @@ def main():
     print("Loading model and processing document...")
     document_path = Path(path)
     chunks = doc_to_chunks(document_path)
-    from sentence_transformers import SentenceTransformer # This import statement happens late because it slows down the program so it happens right before it is necessary
-    transformer_model = SentenceTransformer("all-MiniLM-L6-v2")
-    query_document(transformer_model, chunks, document_path)
+    chroma_client = chromadb.PersistentClient(path="./chromadb") # might need to be a global variable
+    chroma_collection = chroma_client.get_or_create_collection(name=document_path.name.lower())
+    compute_embeddings(chroma_collection, chunks, document_path)
+    query_document(chroma_collection, chunks, document_path)
 
 if __name__ == "__main__":
     main()
